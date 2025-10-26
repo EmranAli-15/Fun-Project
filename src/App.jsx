4277b5c9d32325e966caf62a414a08467635072e
EmranAli-15 <mohammademranali15@gmail.com>
2025-10-26 21:04:49 +0600

    some bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import youtube from "../src/assets/youtubeIcon.svg"
 import facebook from "../src/assets/facebookIcon.svg"
 import insta from "../src/assets/instaIcon.svg"
 import twitter from "../src/assets/twitterIcon.svg"
+import logo from "../src/assets/hikmahLogo.png"
 
 export default function App() {
   const exportRef = useRef(null);
@@ -23,12 +24,12 @@ export default function App() {
   };
 
   return (
-    <div className="max-w-7xl mx-auto fontLan">
+    <div className="max-w-5xl py-8 px-2 mx-auto fontLan">
 
-      <div className="px-2 mt-5 mb-10">
-        {/* Image Upload */}
+      <div className="px-2-5 mb-10">
         <input
           type="file"
+          className="file-input file-input-secondary w-full bg-white text-black border-[#f43098]"
           onChange={(e) => {
             const file = e.target.files[0];
             if (file) setImageURL(URL.createObjectURL(file));
@@ -38,81 +39,81 @@ export default function App() {
         {/* Text Input */}
         <input
           type="text"
-          className="w-full p-1 outline-0 border-1 mt-2"
-          placeholder="Title"
+          placeholder="Headline input"
+          className="input mt-2 w-full bg-white text-black border-[#f43098]"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
 
         <input
           type="text"
-          className="w-full p-1 outline-0 border-1 mt-2"
-          placeholder="date"
+          placeholder="Date input"
+          className="input mt-2 w-full bg-white text-black border-[#f43098]"
           value={date}
           onChange={(e) => setDate(e.target.value)}
         />
       </div>
 
       {/* Preview Box that will be exported */}
-      <div
-        ref={exportRef}
-        className="flex flex-col bg-white"
-      >
-        <div className="flex items-center justify-between my-1 px-2">
-          <div>
-            <h1 className="text-xl font-bold text-blue-600">Hikmah 24</h1>
-            <h1 className="w-10 rounded h-[2px] -mt-1 bg-yellow-300"></h1>
+      <div className="overflow-auto">
+        <div
+          ref={exportRef}
+          className="flex flex-col bg-white max-w-[400px]"
+        >
+          <div className="flex items-center justify-between my-1 px-2">
+            <img className="w-20" src={logo} />
+            <h1 className="text-black">{date}</h1>
           </div>
-          <h1>{date}</h1>
-        </div>
 
 
-        <div className="bg-red-700">
-          <div className="relative h-[250px]">
-            <div className="flex items-center justify-between h-full w-full">
-              <div className="w-[40px] bg-white h-[82%] ml-[6px] rounded-l-2xl"></div>
-              <div className="w-[40px] bg-white h-[82%] mr-[6px] rounded-r-2xl"></div>
+
+          <div className="bg-red-700 px-2">
+            <div className="relative h-80">
+              <div className="flex items-center justify-between h-full w-full">
+                <div className="w-10 bg-white h-[80%] rounded-l-2xl"></div>
+                <div className="w-10 bg-white h-[80%] rounded-r-2xl"></div>
+              </div>
+
+              <div className="absolute top-1/2 w-full -translate-y-1/2">
+                {imageURL &&
+                  <img
+                    src={imageURL}
+                    className="rounded-2xl w-full px-1 object-cover h-[250px]"
+                  />
+                }
+              </div>
             </div>
+            <h1 className="text-white font-sans text-center mb-5 text-xl font-medium">{text}</h1>
+          </div>
 
-            <div className="absolute top-1/2 w-full -translate-y-1/2">
-              {imageURL &&
-                <img
-                  src={imageURL}
-                  className="rounded-[20px] w-full object-cover h-[200px] px-2"
-                />
-              }
+
+
+
+          <div className="flex text-[10px] items-center justify-evenly text-black bg-white">
+            <div className="flex items-center gap-x-1">
+              <img src={facebook} className="size-4" alt="" />
+              <p className="text-[14px]">Hikmah 24</p>
             </div>
-          </div>
-          <h1 className="text-white font-sans text-center mb-5">{text}</h1>
-        </div>
-
-
-
-
-        <div className="flex text-[10px] items-center justify-evenly bg-white">
-          <div className="flex items-center gap-x-1">
-            <img src={facebook} className="size-[12px]" alt="" />
-            <p>Hikmah 24</p>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <img src={youtube} className="size-[12px]" alt="" />
-            <p>Hikmah 24</p>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <img src={insta} className="size-[12px]" alt="" />
-            <p>Hikmah 24</p>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <img src={twitter} className="size-[12px]" alt="" />
-            <p>Hikmah 24</p>
+            <div className="flex items-center gap-x-1">
+              <img src={youtube} className="size-4" alt="" />
+              <p className="text-[14px]">Hikmah 24</p>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <img src={insta} className="size-4" alt="" />
+              <p className="text-[14px]">Hikmah 24</p>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <img src={twitter} className="size-4" alt="" />
+              <p className="text-[14px]">Hikmah 24</p>
+            </div>
           </div>
         </div>
       </div>
 
 
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center mt-5">
         <button
-          className="mt-5 border-red-700 border-1 px-3 py-1 rounded-md"
+          className="btn btn-secondary"
           onClick={handleDownload}
         >
           Download
